515e661f91d20fd24fc1ccda98b84a33b59d835e
BrennanHarrell <s222338763@deakin.edu.au>
2023-09-01 23:37:53 +1000

    new file:   public/style.css 	modified:   src/App.vue 	new file:   src/VueTest.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+  <h1 class="header">Todo App</h1>
+  <div>
+    <input class="newTask" type="text" v-model="todo" placeholder="Create New Todo"/>
+    <button @click="addTask(todo.id)" class="addTask">Add Task</button>
+  </div>
+  <div class="Todo" v-for="todo in todos" v-bind:key="todo.id">
+    <input type="checkbox" v-model="todo.completed" class="taskCompleted">
+    {{ todo.text }}
+    <button @click="removeTask(todo.id)" class="removeTask">Delete</button>
+  </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      todo: "",
+      todos: [ 
+        {
+          id: Math.random(),
+          text: "Test for my Todo List",
+          completed: true,
+          
+        },
+        {
+          id: Math.random(),
+          text: "Hello world",
+          completed: false,
+        }
+
+      ],
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.todo.length === 0) {
+        return;
+      } 
+      else {
+        this.todos.push({
+          id: Math.random(),
+          text: this.todo,
+          completed: false,
+        })
+      }
+    },
+    removeTask(todosId) {
+      this.todos = this.todos.filter((todo) => todo.id !== todosId);
+    },
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "/public/style.css";
 </style>
---
+++ src/VueTest.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {ref} from "vue";
+const show = ref(false);
+const items = ref([{message: "show" }, {message: "v-for" }])
+
+</script>
+<template>
+  <h1>v-if Example </h1>
+  <h2 v-if="show">Button to show off v-if rendering</h2>
+  <h2 v-else>v-else toggled</h2>
+  <button @click="show = !show">Check</button>
+
+  <h1>v-show Example</h1>
+  <h2 v-show="Boolean">Now rendering v-show</h2>
+
+  <h1>v-for Example</h1>
+  <li v-for="item in items" :key="item">
+  {{ item.message }}
+
+  
+  </li>
+
+  <p>Form Input Handling message: {{ message }}</p>
+  <input v-model="message" placeholder="Enter text" /> 
+
+</template>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
